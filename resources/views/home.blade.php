d33288187d03b59f28ed97c20bf66465942dcdeb
Luis Lomeli <luism.lomeli@gmail.com>
2017-08-26 23:59:11 -0600

    Change app name in layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#/">Ventas App</a>
+			<a class="navbar-brand" href="#/">Pharma</a>
 		</div>
 
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
